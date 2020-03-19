76270458a371e027076a4050b92571e1bc50b79a
14138993 <14138993@qq.com>
2020-03-19 11:27:17 +0800

    delete

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,9 @@
-// import search from './search'
-// import table from './table'
-// import tableSetting from './tableSetting'
 import plane from './plane'
 import edit from './edit'
 import box from './box'
 import list from './list'
 export default {
   install (vue) {
-    // vue.component('search', search)
-    // vue.component('vTable', table)
-    // vue.component('tableSetting', tableSetting)
     vue.component('plane', plane)
     vue.component('edit', edit)
     vue.component('box', box)
